07071723c79f8e4bceaf04c5060794a198c0b3fd
Tim Cross <theophilusx@gmail.com>
2018-11-23 14:51:57 +1100

    Refactor git functions

--- src/git.js
+++ src/git.js
@@ -22,19 +22,98 @@ const cloneOptions = {
   } 
 };
 
-function cloneRepo(repoUrl, dst) {
-  const logName = `${moduleName}.cloneRepo`;
+function getRepository(repoUrl, dst) {
+  const logName = `${moduleName}.getRepository`;
   
   return Git.Clone(repoUrl, dst, cloneOptions)
     .then(repo => {
-      console.dir(repo);
       return repo;
     })
     .catch(err => {
+      if (err.errno === -4) {
+        // looks like repo already there - try opening and pull
+        console.log(`Repository ${dst} already exists`);
+        return Git.Repository.open(dst)
+          .catch(err => {
+            throw new VError(err, `${logName} Failed to open repo ${dst}`);
+          });
+      }
       throw new VError(err, `${logName} Failed to clone ${repoUrl}`);
     });
 }
 
+
+function statusString(s) {
+  let words = [];
+  if (s.isConflicted()) {
+    words.push("CONFLICT");
+  }
+  if (s.isDeleted()) {
+    words.push("DELETED");
+  }
+  if (s.isIgnored()) {
+    words.push("IGNORED");
+  }
+  if (s.isModified()) {
+    words.push("MODIFIED");
+  }
+  if (s.isNew()) {
+    words.push("NEW");
+  }
+  if (s.isRenamed()) {
+    words.push("MOVED");
+  }
+  if (s.isTypechange()) {
+    words.push("TYPECHANGE");
+  }
+  return `${words.join(" ")} ${s.path()} ${s.status()}`; 
+}
+
+function getReferenceNames(repo) {
+  const logName = `${moduleName}.getReferences`;
+
+  return repo.getReferenceNames(Git.Reference.TYPE.LISTALL)
+    .then(names => {
+      names.forEach(refName => {
+        console.log(`Reference: ${refName}`);
+      });
+      return names;
+    })
+    .catch(err => {
+      throw new VError(err, `${logName} Failed to get references`);
+    });
+}
+
+function setupRepository(repoUrl, repoDest) {
+  const logName = `${moduleName}.setupRepository`;
+  let repo;
+  return getRepository(repoUrl, repoDest)
+    .then(r => {
+      repo = r;
+      return repo.getStatus();
+    })
+    .then(statusList => {
+      if (statusList.length) {
+        console.log("Uncommited changes exist in repository");
+        statusList.forEach(f => console.log(statusString(f)));
+      } else {
+        console.log("Nothing needing to be committed - do pull");
+      }
+    })
+    .then(() => {
+      return getReferenceNames(repo);
+    })
+    .then(() => {
+      return repo;
+    })
+    .catch(err => {
+      throw new VError(err, `${logName} Failed to setup ${repoDest}`);
+    });
+}
+
+
 module.exports = {
-  cloneRepo 
+  statusString,
+  getReferenceNames,
+  setupRepository
 };
--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ const config = require("rc")(appName);
 const repoName = "casi-admin-db";
 const repoUrl = config.dbRepositories[repoName];
 
-git.cloneRepo(repoUrl, path.join(config.dbcmHome, repoName))
+
+git.setupRepository(repoUrl, path.join(config.dbcmHome, repoName))
   .then(repo => {
     console.log("All good");
+    return repo;
   })
   .catch(err => {
     console.error(err.message);
