b77b1cb10ffec6f84048ee0c839c517f07cdf0de
Tim Cross <theophilusx@gmail.com>
2018-11-23 16:22:00 +1100

    Add first take of addAndCommit() function

--- src/git.js
+++ src/git.js
@@ -98,6 +98,46 @@ function getReferenceNames(repo) {
     });
 }
 
+function addAndCommit(repo, files) {
+  const logName = `${moduleName}.addAndCommit`;
+  let index, oid;
+  
+  return repo.checkoutBranch("dbcm-local")
+    .then(() => {
+      return repo.refreshIndex();
+    })
+    .then(idx => {
+      let promises = [];
+      index = idx;
+      files.forEach(f => {
+        promises.push(index.addByPath(f.path()));
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      return index.write();
+    })
+    .then(() => {
+      return index.writeTree();
+    })
+    .then((newOid) => {
+      oid = newOid;
+      return Git.Reference.nameToId(repo, "HEAD");
+    })
+    .then(head => {
+      return repo.getCommit(head);
+    })
+    .then(parent => {
+      let author = Git.Signature.now("Some Author", "author@example.com");
+      let committer = Git.Signature.now("Some Committer", "committer@exaple.com");
+      let msg = "Initial setup";
+      return repo.createCommit("HEAD", author, committer, msg, oid, [parent]);
+    })
+    .catch(err => {
+      throw new VError(err, `${logName} Failed to commit changes`);
+    });
+}
+
 function setupRepository(repoUrl, repoDest) {
   const logName = `${moduleName}.setupRepository`;
   let repo;
@@ -110,6 +150,10 @@ function setupRepository(repoUrl, repoDest) {
       if (statusList.length) {
         console.log("Uncommited changes exist in repository");
         statusList.forEach(f => console.log(statusString(f)));
+        return addAndCommit(repo, statusList)
+          .then(() => {
+            return pullMaster(repo);
+          });
       } else {
         console.log("Nothing needing to be committed - do pull");
       }
