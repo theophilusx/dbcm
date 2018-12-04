7d5d4d0dd12434b988e762a86c94210a871e43c2
Tim Cross <theophilusx@gmail.com>
2018-12-04 13:52:22 +1100

    Add textMenu module

---
+++ src/textMenus.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const moduleName = "textMenus";
+
+const VError = require("verror");
+const inquirer = require("inquirer");
+
+function buildChoices(choiceData) {
+  const logName = `${moduleName}.buildChoices`;
+
+  try {
+    let choices = [];
+    for (let c of choiceData) {
+      if (Array.isArray(c)) {
+        choices.push({
+          name: c[0],
+          value: c[1]
+        });
+      } else {
+        choices.push(c);
+      }
+    }
+    choices.push(new inquirer.Separator());
+    choices.push({
+      name: "Leave menu",
+      value: "exitMenu"
+    });
+    choices.push({
+      name: "Exit DBCM",
+      value: "exitProgram"
+    });
+    return choices;
+  } catch (err) {
+    throw new VError(err, `${logName} Failed to build choice list`);
+  }
+}
+
+function displayMenu(title, question, actionFN) {
+  const logName = `${moduleName}.displayMenu`;
+
+  console.log(`\n\t${title}\n`);
+  return inquirer.prompt(question)
+    .then(actionFN)
+    .catch(err => {
+      throw new VError(err, `${logName} Failed to display menu ${title}`);
+    });
+}
+
+function defaultAction(answer) {
+  const logName = `${moduleName}.defaultAction`;
+
+  try {
+    let finished = false;
+    let choice = answer.choice;
+    if (doExit(choice)) {
+      finished = true;
+      return [finished, choice];
+    }
+    return [finished, choice];
+  } catch (err) {
+    throw new VError(err, `${logName} Error in default menu action`);
+  }
+}
+
+function displayListMenu(title, prompt, choices, actionFN = defaultAction) {
+  const logName = `${moduleName}.displayListMenu`;
+
+  let question = [{
+    type: "list",
+    name: "choice",
+    choices: choices,
+    message: prompt
+  }];
+  
+  return displayMenu(title, question, actionFN)
+    .catch(err => {
+      throw new VError(err, `${logName} Failed to display list menu`);
+    });
+}
+
+function doExit(choice) {
+  if (choice === "exitMenu" || choice === "exitProgram") {
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  buildChoices,
+  displayMenu,
+  displayListMenu,
+  doExit
+};
