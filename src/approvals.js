f057e70c9cadb42ccf1a4764fba23d79d9bac1d8
Tim Cross <theophilusx@gmail.com>
2018-12-05 13:14:13 +1100

    Updagted approvals module to use state object

--- src/approvals.js
+++ src/approvals.js
@@ -6,9 +6,9 @@ const VError = require("verror");
 const path = require("path");
 const fse = require("fse");
 
-function readApprovalsFile(appState) {
+function readApprovalsFile(state) {
   const logName = `${moduleName}.readApprovalsFile`;
-  const filePath = path.join(appState.get("home"), appState.get("currentRepository"), "approvals.json");
+  const filePath = path.join(state.home, state.currentRepository, "approvals.json");
 
   return fse.readJson(filePath)
     .then(data => {
@@ -16,25 +16,25 @@ function readApprovalsFile(appState) {
       for (let ap of data.approvers) {
         approverMap.set(ap.email, ap.name);
       }
-      appState.set("approvalType", data.type);
-      appState.set("approvers", approverMap);
-      return appState;
+      state.setApproverType(data.type); 
+      state.setApprovers(approverMap);
+      return state;
     })
     .catch(err => {
       throw new VError(err, `${logName} Failed to read approvals.json`);
     });
 }
 
-function writeApprovalsFile(appState) {
+function writeApprovalsFile(state) {
   const logName = `${moduleName}.writeApprovalsFile`;
-  const filePath = path.join(appState.get("home"), appState.get("currentRepository"), "approvals.json");
+  const filePath = path.join(state.home, state.currentRepository, "approvals.json");
   let content = {
     name: "Change Approvals",
     version: "1.0.0",
-    type: appState.get("approvalType")
+    type: state.approvalType
   };
   let approvers = [];
-  let appMap = appState.get("approvers");
+  let appMap = state.approvers;
   
   for (let email of appMap.keys()) {
     approvers.push({
