d6acd9f563bea752ee51602a9e8c406c8fa48288
Tim Cross <theophilusx@gmail.com>
2018-12-09 09:39:12 +1100

    Update SQL DDL to reflect plan rather than set

--- sql/dbcm-init.sql
+++ sql/dbcm-init.sql
@@ -7,10 +7,8 @@ CREATE ROLE dbcm_user
 
 CREATE SCHEMA dbcm AUTHORIZATION dbcm_user;
 
-CREATE TYPE change_status AS ENUM ('applied', 'verified', 'rolledback', 'invalid', 'unknown');
-
-CREATE TABLE dbcm.change_sets (
-    set_id VARCHAR(22) PRIMARY KEY 
+CREATE TABLE dbcm.change_plans (
+    plan_id VARCHAR(22) PRIMARY KEY 
   , applied_dt TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
   , applied_by VARCHAR(63) NOT NULL  
   , verified_dt TIMESTAMPTZ
@@ -19,16 +17,16 @@ CREATE TABLE dbcm.change_sets (
   , rollback_by VARCHAR(63)
   , set_name VARCHAR(64) NOT NULL
   , description TEXT
-  , status change_status
+  , status VARCHAR(32)
 );
 
 CREATE TABLE dbcm.change_log (
     log_id SERIAL
   , log_dt TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-  , set_id VARCHAR(22) NOT NULL
-  , set_name VARCHAR(64) NOT NULL
+  , plan_id VARCHAR(22) NOT NULL
+  , plan_name VARCHAR(64) NOT NULL
   , msg TEXT
-  , CONSTRAINT change_log_pkey PRIMARY KEY (log_id, set_id)
+  , CONSTRAINT change_log_pkey PRIMARY KEY (log_id, plan_id)
 );
 
 COMMIT;
