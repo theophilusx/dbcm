0d22340dfad4780f636456793b8d2f8dc42fff51
Tim Cross <theophilusx@gmail.com>
2018-12-13 17:00:16 +1100

    Change table formats for dbcm schema

--- sql/dbcm-init.sql
+++ sql/dbcm-init.sql
@@ -8,7 +8,7 @@ CREATE ROLE dbcm_user
 CREATE SCHEMA dbcm AUTHORIZATION dbcm_user;
 
 CREATE TABLE dbcm.change_plans (
-    plan_id VARCHAR(22) PRIMARY KEY 
+    plan_id VARCHAR(22) NOT NULL
   , applied_dt TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
   , applied_by VARCHAR(64) NOT NULL  
   , verified_dt TIMESTAMPTZ
@@ -17,7 +17,11 @@ CREATE TABLE dbcm.change_plans (
   , rollback_by VARCHAR(64)
   , plan_name VARCHAR(64) NOT NULL
   , description TEXT
-  , status VARCHAR(32)
+  , plan_type VARCHAR(24) NOT NULL DEFAULT 'devleopment'
+  , repository_version VARCHAR(16)
+  , change_sha VARCHAR(40) NOT NULL 
+  , status VARCHAR(16)
+  , CONSTRAINT change_plans_pkey PRIMARY KEY (plan_id, repository_version)
 );
 
 CREATE TABLE dbcm.change_log (
