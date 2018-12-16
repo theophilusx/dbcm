f0769cd32063c4384fb2ced7dbd9ffc316fef2df
Tim Cross <theophilusx@gmail.com>
2018-12-17 07:16:51 +1100

    Handle conflicts in files - basic

--- src/gitUI.js
+++ src/gitUI.js
@@ -6,6 +6,14 @@ const VError = require("verror");
 const inquirer = require("inquirer");
 const git = require("./git");
 
+function getConflictedChange(fileList) {
+  let conflictFiles = fileList.map(f => f.isConflicted());
+  if (conflictFiles.length) {
+    return conflictFiles.map(f => f.path());
+  }
+  return [];
+}
+
 async function commitChanges(state) {
   const logName = `${moduleName}.commitChanges`;
 
@@ -13,11 +21,21 @@ async function commitChanges(state) {
     let repo = state.get("repoObject");
     let files = await repo.getStatus();
     if (files.length) {
+      let conflicts = getConflictedChange(files);
+      if (conflicts.length) {
+        screen.warningMsg(
+          "Resolve Conflicts",
+          "The following files have conflicting changes which must be resolved manually\n"
+            + conflicts.join("\n")
+        );
+        return false;
+      }
       let changeStrings = files.map(f => git.statusString(f));
       console.log(`Changed File Status - ${files.length} file(s)\n`);
       for (let c of changeStrings) {
         console.log(c);
       }
+      console.log("");
       let answer = await inquirer.prompt([
         {
           type: "confirm",
