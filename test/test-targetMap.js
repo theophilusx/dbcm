93aeb11332076fff1afb54e22cb5febc716b3972
Tim Cross <theophilusx@gmail.com>
2019-01-08 08:50:36 +1100

    Extended TargetMap tests

--- test/test-targetMap.js
+++ test/test-targetMap.js
@@ -5,6 +5,20 @@ const should = require("chai").should();
 const {Target} = require("../src/Target");
 const {TargetMap} = require("../src/TargetMap");
 
+let testMap, newMap;
+let testTarget1, testTarget2, testTarget3;
+let testTarget4, testTarget5, testTarget6;
+
+function initTestData() {
+  testTarget1 = new Target("targetName1", "db1", "user1", "pwd1");
+  testTarget2 = new Target("targetName2", "db2", "user2", "pwd2");
+  testTarget3 = new Target("targetName3", "db3", "user3", "pwd3");
+  testTarget4 = new Target("targetName4", "db4", "user4", "pwd4");
+  testTarget5 = new Target("targetName5", "db5", "user5", "pwd5");
+  testTarget6 = new Target("targetName6", "db6", "user6", "pwd6");
+  testMap = new TargetMap([testTarget1, testTarget2]);
+}
+
 describe("Test TargetMap object", function() {
 
   describe("Plain constructor called", function() {
@@ -18,40 +32,39 @@ describe("Test TargetMap object", function() {
   });
 
   describe("Constructor called with arguments", function() {
-    let t1 = new Target("tName1", "dbName1", "dbUser1", "dbPwd1");
-    let t2 = new Target("tName2", "dbName2", "dbUser2", "dbPwd2");
-    let tm = new TargetMap([t1, t2]);
+    before("Initialise test data", function() {
+      initTestData();
+    });
 
     it("Initialised object creation", function() {
-      expect(tm).to.be.an("object");
+      expect(testMap).to.be.an("object");
     });
 
     it("Object has correct properties", function() {
-      expect(tm).to.have.key("targets");      
+      expect(testMap).to.have.key("targets");      
     });
 
     it("Object has correct number of elements", function() {
-      expect(tm.targets.size).to.equal(2);      
+      expect(testMap.targets.size).to.equal(2);      
     });
   });
   
   describe("Test TargetMap methods", function() {
-    let t1 = new Target("tName1", "dbName1", "dbUser1", "dbPwd1");
-    let t2 = new Target("tName2", "dbName2", "dbUser2", "dbPwd2");
-    let t3 = new Target("tName3", "dbName3", "dbUser3", "dbPwd3");
-    let tm = new TargetMap([t1, t2]);
+    before("Initalise test data", function() {
+      initTestData();
+    });
 
     describe("Get method", function() {
       it("Non existent key returns undefined", function() {
-        expect(tm.get("notExist")).to.equal(undefined);      
+        expect(testMap.get("notExist")).to.equal(undefined);      
       });
 
       it("Existing key returns target object", function() {
-        expect(tm.get("tName1")).to.be.an("object");
+        expect(testMap.get("targetName1")).to.be.an("object");
       });
 
       it("Returned object has correct properties", function() {
-        expect(tm.get("tName1")).to.include.all.keys(
+        expect(testMap.get("targetName2")).to.include.all.keys(
           "name",
           "database",
           "host",
@@ -62,24 +75,26 @@ describe("Test TargetMap object", function() {
       });
 
       it("Returned object has same values as initialiser object", function() {
-        expect(tm.get("tName2")).to.deep.equal(t2);        
+        expect(testMap.get("targetName2")).to.deep.equal(testTarget2);        
       });
     });
 
     describe("Set method", function() {
       describe("Add new target", function() {
-        tm.set(t3);
+        before("Add new test target", function() {
+          testMap.set(testTarget3);
+        });
 
         it("Targets increased by 1", function() {
-          expect(tm.targets.size).to.equal(3);        
+          expect(testMap.targets.size).to.equal(3);        
         });
 
         it("Can retrieve new target", function() {
-          expect(tm.get("tName3")).to.be.an("object");        
+          expect(testMap.get("targetName3")).to.be.an("object");        
         });
 
         it("Verify returned target has right keys", function() {
-          expect(tm.get("tName3")).to.include.all.keys(
+          expect(testMap.get("targetName3")).to.include.all.keys(
             "name",
             "database",
             "host",
@@ -88,68 +103,85 @@ describe("Test TargetMap object", function() {
             "password"
           );
         });
+
         it("Verify returns correct target", function() {
-          expect(tm.get("tName3")).to.deep.equal(t3);
+          expect(testMap.get("targetName3")).to.deep.equal(testTarget3);
         });
       });
     });
     
     describe("names method", function() {
       it("names returns array", function() {
-        expect(tm.names()).to.be.an("array");
+        expect(testMap.names()).to.be.an("array");
       });
 
       it("Names return length is correct", function() {
-        expect(tm.names().length).to.equal(3);
+        expect(testMap.names().length).to.equal(3);
       });
 
       it("Names returns all names", function() {
-        expect(tm.names()).includes("tName1", "tName2", "tName3");
+        expect(testMap.names()).includes("targetName1", "targetName2", "targetName3");
       });
     });
     
     describe("toArray method", function() {
 
       it("Returns an array", function() {
-        expect(tm.toArray()).is.an("array");
+        expect(testMap.toArray()).is.an("array");
       });
 
       it("Has correct number of elements", function() {
-        expect(tm.toArray().length).to.equal(3);
+        expect(testMap.toArray().length).to.equal(3);
       });
 
       it("Has correct elements", function() {
-        expect(tm.toArray()).to.deep.equal([t1, t2, t3]);
+        expect(testMap.toArray()).to.deep.equal([testTarget1, testTarget2, testTarget3]);
       });
     });
 
     describe("targetParams()", function() {
 
       it("returns object", function() {
-        expect(tm.targetParams("tName1")).to.be.an("object");
+        expect(testMap.targetParams("targetName1")).to.be.an("object");
       });
 
       it("returns correct params", function() {
-        expect(tm.targetParams("tName2")).to.deep.equal(t2.params());
+        expect(testMap.targetParams("targetName2")).to.deep.equal(testTarget2.params());
       });
     });
 
     describe("fromArrayu method", function() {
-      let b1 = new Target("n1", "db1", "u1", "pwd1");
-      let b2 = new Target("n2", "db2", "u2", "pwd2");
-      let b3 = new Target("n3", "db3", "u3", "pwd3");
-
-      it("Re-initialise object", function() {
-        expect(tm.fromArray([b1, b2, b3])).to.be.an("object");
+      before("Re-initialise target map", function() {
+        testMap.fromArray([testTarget4, testTarget5, testTarget6]);
       });
 
-      it("Has 3 targets", function() {
-        expect(tm.targets.size).to.equal(3);
+      it("Targets has correct number of elements", function() {
+        expect(testMap.targets.size).to.equal(3);
       });
 
       it("has correct elements", function() {
-        expect(tm.toArray()).to.deep.equal([b1, b2, b3]);
+        expect(testMap.toArray()).to.deep.equal([
+          testTarget4,
+          testTarget5,
+          testTarget6
+        ]);
       });
+    });
+  });
+
+  describe("Test TargetMap -> array -> TargetMap", function() {
+    before("Initialise for test", function() {
+      initTestData();
+      newMap = new TargetMap();
+      newMap.fromArray(testMap.toArray());
+    });
+
+    it("Has correct number of targets", function() {
+      expect(newMap.targets.size).to.equal(testMap.targets.size);
+    });
+
+    it("Has correct target elements", function() {
+      expect(newMap.toArray()).to.deep.equal(testMap.toArray());
     });
   });
 });
