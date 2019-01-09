86ae3c4ec487b9d2d95a19f9e19d9b6c7a59ade4
Tim Cross <theophilusx@gmail.com>
2019-01-09 13:26:43 +1100

    Modified Plan to use ApprovalHistory object

--- src/Plan.js
+++ src/Plan.js
@@ -6,6 +6,7 @@ const VError = require("verror");
 const moment = require("moment");
 const path = require("path");
 const short = require("short-uuid");
+const {ApprovalHistory} = require("./ApprovalHistory");
 
 function Plan(initData) {
   const logName = `${moduleName}.Plan`;
@@ -21,9 +22,7 @@ function Plan(initData) {
       this.author = initData.author;
       this.authorEmail = initData.email;
       this.planType = initData.planType ? initData.planType : "Development";
-      this.approved = initData.approved ? initData.approved : false;
-      this.approvalSha = initData.approvalSha;
-      this.approvals = initData.approvals;
+      this.approvals = new ApprovalHistory(),
       this.change = path.join("change", `${initData.name.replace(/\s+/g, "-")}.sql`);
       this.verify = path.join("verify", `${initData.name.replace(/\s+/g, "-")}.sql`);
       this.rollback = path.join("rollback", `${initData.name.replace(/\s+/g, "-")}.sql`);
@@ -46,6 +45,46 @@ Plan.prototype.setType = function(type) {
     throw new Error(`Unknown plan type ${type}`);
   } catch (err) {
     throw new VError(err, `${logName} Failed to set plan type`);
+  }
+};
+
+Plan.prototype.currentApproval = function() {
+  return this.approvals.currentApproval();
+};
+
+Plan.protothype.currentApprovalState = function() {
+  return this.approvals.currentApprovalState();
+};
+
+Plan.prototyupe.currentApprovalCount = function() {
+  return this.approvals.currentApprovalCount();
+};
+
+Plan.protothype.addApproval = function(author, email, sha) {
+  return this.approvals.addApproval(author, email, sha);
+};
+
+Plan.prototype.resetApproval = function() {
+  return this.approvals.resetApproval();
+};
+
+Plan.prototype.fromObject = function(pObj) {
+  const logName = `${moduleName}.fromObject`;
+
+  try {
+    this.uuid = pObj.uuid;
+    this.name = pObj.name;
+    this.description = pObj.description;
+    this.createdDate = pObj.createdDate;
+    this.author = pObj.author;
+    this.authorEmail = pObj.authorEmail;
+    this.planType = pObj.planType;
+    this.approvals.fromObject(pObj.approvals);
+    this.change = pObj.change;
+    this.verify = pObj.verify;
+    this.rollback = pObj.rollback;
+  } catch (err) {
+    throw new VError(err, `${logName} Failed to create plan from object`);
   }
 };
 
