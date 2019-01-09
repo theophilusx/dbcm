d3b52025cb398e887bbecdb63230630f4ab4d797
Tim Cross <theophilusx@gmail.com>
2019-01-09 13:40:01 +1100

    Added PlanMap module

---
+++ src/PlanMap.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const moduleName = "PlanMap";
+
+const {Plan} = require("./Plan");
+
+function PlanMap() {
+  this.plans = new Map();
+}
+
+PlanMap.prototype.add = function(plan) {
+  this.plans.set(plan.uuid, plan);
+};
+
+PlanMap.prototype.get = function(key) {
+  return this.plans.get(key);
+};
+
+PlanMap.prototype.toObject = function() {
+  let plans = [];
+  for (let p of this.plans.keys()) {
+    plans.push(this.plans.get(p));
+  }
+};
+
+PlanMap.prototype.fromObject = function(pList) {
+  pList.forEach(p => {
+    let plan = new Plan(p);
+    this.plans.set(plan.uuid, plan);
+  });
+};
