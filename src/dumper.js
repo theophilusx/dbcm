2bce04356e387b9a3d4b21236ae51cf0a880d408
Tim Cross <theophilusx@gmail.com>
2019-01-26 19:33:00 +1100

    Fix require statement for GitRepo

--- src/dumper.js
+++ src/dumper.js
@@ -2,8 +2,9 @@
 
 const moduleName = "dumper";
 
+const VError = require("verror");
 const AppState = require("./AppState");
-const {GitRepo} = require("./GitRepo");
+const GitRepo = require("./GitRepo");
 const Plan = require("./Plan");
 const PlanMap = require("./PlanMap");
 const Repository = require("./Repository");
@@ -13,43 +14,48 @@ const TargetMap = require("./TargetMap");
 
 function getType(v) {
   const logName = `${moduleName}.getType`;
-  
-  switch (typeof v) {
-  case "boolean":
-  case "number":
-  case "string":
-  case "symbol":
-  case "undefined":
-  case "function":
-    return typeof v;
-  case "object":
-    if (Array.isArray(v)) {
-      return "array";
-    } else if (v instanceof AppState) {
-      return "AppState";
-    } else if (v instanceof GitRepo) {
-      return "GitRepo";
-    } else if (v instanceof Plan) {
-      return "Plan";
-    } else if (v instanceof PlanMap) {
-      return "PlanMap";
-    } else if (v instanceof Repository) {
-      return "Repository";
-    } else if (v instanceof RepositoryMap) {
-      return "RepositoryMap";
-    } else if (v instanceof Target) {
-      return "Target";
-    } else if (v instanceof TargetMap) {
-      return "TargetMap";
-    } else if (v instanceof Map) {
-      return "map";
-    } else if (v instanceof Set) {
-      return "set";
-    } else {
-      return "object";
+
+  try {
+    switch (typeof v) {
+    case "boolean":
+    case "number":
+    case "string":
+    case "symbol":
+    case "undefined":
+    case "function":
+      return typeof v;
+    case "object":
+      if (Array.isArray(v)) {
+        return "array";
+      } else if (v instanceof AppState) {
+        return "AppState";
+      } else if (v instanceof GitRepo) {
+        return "GitRepo";
+      } else if (v instanceof Plan) {
+        return "Plan";
+      } else if (v instanceof PlanMap) {
+        return "PlanMap";
+      } else if (v instanceof Repository) {
+        return "Repository";
+      } else if (v instanceof RepositoryMap) {
+        return "RepositoryMap";
+      } else if (v instanceof Target) {
+        return "Target";
+      } else if (v instanceof TargetMap) {
+        return "TargetMap";
+      } else if (v instanceof Map) {
+        return "map";
+      } else if (v instanceof Set) {
+        return "set";
+      } else {
+        return "object";
+      }
+    default:
+      throw new Error(`${logName} Unrecognised object type: ${v}`);
     }
-  default:
-    throw new Error(`${logName} Unrecognised object type: ${v}`);
+  } catch (err) {
+    console.dir(v);
+    throw new VError(err, `${logName} value ${v}`);
   }
 }
 
@@ -78,49 +84,55 @@ function dumpFunction(v) {
 }
 
 function dumpValue(v, indent="", name="") {
+  const logName = `${moduleName}.dumpValue`;
   const type = getType(v);
-  switch(type) {
-  case "boolean":
-    return `${indent}${name} ${dumpBoolean(v)}`;
-  case "number":
-    return `${indent}${name} ${dumpNumber(v)}`;
-  case "string":
-    return `${indent}${name} ${dumpString(v)}`;
-  case "symbol":
-    return `${indent}${name} ${dumpSymbol(v)}`;
-  case "undefined":
-    return `${indent}${name} ${dumpUndefined(v)}`;
-  case "function":
-    return `${indent}${name} ${dumpFunction(v)}`;
-  case "array": {
-    let data = [];
-    let cnt = 0;
-    data.push(`${indent}${name} [array] [`);
-    v.forEach(el => {
-      data.push(dumpValue(el, indent + " ", `${cnt}:`));
-      cnt++;
-    });
-    data.push(`${indent}]`);
-    return data.join("\n");
-  }
-  case "map": {
-    let data = [];
-    data.push(`${indent}${name} [map] {`);
-    for (let [k, value] of v.entries()) {
-      data.push(dumpValue(value, `${indent} `, k));
+
+  try {
+    switch(type) {
+    case "boolean":
+      return `${indent}${name} ${dumpBoolean(v)}`;
+    case "number":
+      return `${indent}${name} ${dumpNumber(v)}`;
+    case "string":
+      return `${indent}${name} ${dumpString(v)}`;
+    case "symbol":
+      return `${indent}${name} ${dumpSymbol(v)}`;
+    case "undefined":
+      return `${indent}${name} ${dumpUndefined(v)}`;
+    case "function":
+      return `${indent}${name} ${dumpFunction(v)}`;
+    case "array": {
+      let data = [];
+      let cnt = 0;
+      data.push(`${indent}${name} [array] [`);
+      v.forEach(el => {
+        data.push(dumpValue(el, indent + " ", `${cnt}:`));
+        cnt++;
+      });
+      data.push(`${indent}]`);
+      return data.join("\n");
     }
-    data.push(`${indent}}`);
-    return data.join("\n");
-  }
-  default: {
-    let data = [];
-    data.push(`${indent}${name} [${type}] {`);
-    for (let [p, value] of Object.entries(v)) {
-      data.push(dumpValue(value, `${indent} `, p));
+    case "map": {
+      let data = [];
+      data.push(`${indent}${name} [map] {`);
+      for (let [k, value] of v.entries()) {
+        data.push(dumpValue(value, `${indent} `, k));
+      }
+      data.push(`${indent}}`);
+      return data.join("\n");
     }
-    data.push(`${indent}}`);
-    return data.join("\n");
-  }
+    default: {
+      let data = [];
+      data.push(`${indent}${name} [${type}] {`);
+      for (let [p, value] of Object.entries(v)) {
+        data.push(dumpValue(value, `${indent} `, p));
+      }
+      data.push(`${indent}}`);
+      return data.join("\n");
+    }
+    }
+  } catch (err) {
+    throw new VError(err, `${logName}`);
   }
 }
 
