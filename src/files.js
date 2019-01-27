d0d2c51b043bea2bf0ec0196b13e9bcc2b1907cb
Tim Cross <theophilusx@gmail.com>
2019-01-27 15:18:34 +1100

    Update initialiseRepo for new format

--- src/files.js
+++ src/files.js
@@ -6,78 +6,30 @@ const VError = require("verror");
 const path = require("path");
 const fse = require("fse");
 
-function createPlanFiles(rootPath) {
+function createPlanFile(rootPath) {
   const logName = `${moduleName}.createPlanFiles`;
-  const approvedPlans = {
-    name: "Approved Plans",
+  const contents = {
+    name: "Change Plans",
     version: "1.0.0",
     plans: []
   };
-  const pendingPlans = {
-    name: "Pending Plans",
-    version: "1.0.0",
-    plans: []
-  };
-  const developmentPlans = {
-    name: "Development Plans",
-    version: "1.0.0",
-    plans: []
-  };
-  const rejectedPlans = {
-    name: "Rejected Plans",
-    version: "1.0.0",
-    plans: []
-  };
-  const approvedFile = path.join(rootPath, "approved-plans.json");
-  const rejectedFile = path.join(rootPath, "rejected-plans.json");
-  const pendingFile = path.join(rootPath, "pending-plans.json");
-  const developmentFile = path.join(rootPath, "development-plans.json");
+  const planFile = path.join(rootPath, "change-plans.json");
 
-  return fse.writeFile(approvedFile, JSON.stringify(approvedPlans, null, " "), "utf-8")
+  return fse.writeFile(planFile, JSON.stringify(contents, null, " "), "utf-8")
     .then(() => {
-      console.log("Created approved-plans.json file");
-      return fse.writeFile(pendingFile, JSON.stringify(pendingPlans, null, " "), "utf-8");
-    })
-    .then(() => {
-      console.log("Created pending-plans.json file");
-      return fse.writeFile(developmentFile, JSON.stringify(developmentPlans, null, " "), "utf-8");
-    })
-    .then(() => {
-      console.log("Created development-plans.json");
-      return fse.writeFile(rejectedFile, JSON.stringify(rejectedPlans, null, " "), "utf-8");;
-    })
-    .then(() => {
-      console.log("Created rejected-plans.json");
+      console.log("Created change-plans.json file");
       return true;
     })
     .catch(err => {
-      throw new VError(err, `${logName} Failed to create plans.json file in ${rootPath}`);
-    });
-}
-
-function createApproverFile(rootPath) {
-  const logName = `${moduleName}.createApproverFile`;
-  const approverFile = path.join(rootPath, "approvals.json");
-  const content = {
-    name: "Change Approvals",
-    version: "1.0.0",
-    type: "none",
-    approvers: []
-  };
-
-  return fse.writeFile(approverFile, JSON.stringify(content, null, " "), "utf-8")
-    .then(() => {
-      console.log("Created approvals.json");
-      return true;
-    })
-    .catch(err => {
-      throw new VError(err, `${logName} Failed to create approvals.json file`);
+      throw new VError(err, `${logName} Failed to create change-plans.json file `
+                       + `in ${rootPath}`);
     });
 }
 
 function createChangesDir(rootPath) {
   const logName = `${moduleName}.createChangesDir`;
-  const contents = "This directory contains SQL scripts representing DB structure changes";
+  const contents =
+        "This directory contains SQL scripts representing DB structure changes";
   const dir = path.join(rootPath, "changes");
   
   return fse.mkdir(dir)
@@ -90,13 +42,15 @@ function createChangesDir(rootPath) {
       return true;
     })
     .catch(err => {
-      throw new VError(err, `${logName} Failed to create changes directory in ${rootPath}`);
+      throw new VError(err, `${logName} Failed to create changes directory `
+                       + `in ${rootPath}`);
     });
 }
 
 function createVerifyDir(rootPath) {
   const logName = `${moduleName}.createVerifyDir`;
-  const contents = "This directory contains SQL scripts to verify DB structure changes";
+  const contents =
+        "This directory contains SQL scripts to verify DB structure changes";
   const dir = path.join(rootPath, "verify");
   
   return fse.mkdir(dir)
@@ -109,13 +63,15 @@ function createVerifyDir(rootPath) {
       return true;
     })
     .catch(err => {
-      throw new VError(err, `${logName} Failed to create verify directory in ${rootPath}`);
+      throw new VError(err, `${logName} Failed to create verify directory`
+                       + ` in ${rootPath}`);
     });
 }
 
 function createRollbackDir(rootPath) {
   const logName = `${moduleName}.createRollbackDir`;
-  const contents = "This directory contains SQL scripts to rollback DB structure changes";
+  const contents =
+        "This directory contains SQL scripts to rollback DB structure changes";
   const dir = path.join(rootPath, "rollback");
   
   return fse.mkdir(dir)
@@ -128,14 +84,16 @@ function createRollbackDir(rootPath) {
       return true;
     })
     .catch(err => {
-      throw new VError(err, `${logName} Failed to create rollback directory in ${rootPath}`);
+      throw new VError(err, `${logName} Failed to create rollback directory `
+                       + `in ${rootPath}`);
     });
 }
 
 function isInitialised(rootPath) {
   const logName = `${moduleName}.isInitialised`;
-
-  return fse.access(path.join(rootPath, "approved-plans.json"), fse.constants.R_OK | fse.constants.W_OK)
+  const planFile = path.join(rootPath, "change-plans.json");
+  
+  return fse.access(planFile, fse.constants.R_OK | fse.constants.W_OK)
     .then(() => {
       return true;
     })
@@ -151,8 +109,7 @@ async function initialiseRepo(rootPath) {
   const logName = `${moduleName}.initialiseRepo`;
 
   try {
-    await createPlanFiles(rootPath);
-    await createApproverFile(rootPath);
+    await createPlanFile(rootPath);
     await createChangesDir(rootPath);
     await createVerifyDir(rootPath);
     await createRollbackDir(rootPath);
