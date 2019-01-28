4d4c463dcdb35e07093056187d4d2d531822453b
Tim Cross <theophilusx@gmail.com>
2019-01-28 17:22:51 +1100

    Refine screen box messages

--- src/tui/textScreen.js
+++ src/tui/textScreen.js
@@ -7,20 +7,21 @@ const wrapAnsi = require("wrap-ansi");
 
 const boxOptions = {
   padding: 1,
+  margin: 1,
   float: "center",
   align: "left"
 };
 
 function menuHeading(txt) {
   let width = cliWidth({defaultWidth: 80});
-  let paddingSize = (width - txt.length) / 2;
+  let paddingSize = Math.floor((width - txt.length) / 2);
   let padding = " ".repeat(paddingSize);
   console.log(`${padding}${chalk.underline.yellow(txt)}\n`);
 }
 
 function heading(txt) {
   let width = cliWidth({defaultWidth: 80});
-  let paddingSize = (width - txt.length) / 2;
+  let paddingSize = Math.floor((width - txt.length) / 2);
   let padding = " ".repeat(paddingSize);
   console.log(`${padding}${chalk.underline.cyan(txt)}\n`);
 }
@@ -39,27 +40,22 @@ function status(state) {
 }
 
 function errorMsg(title, msg) {
-  let width = cliWidth({defaultWidth: 80});
-  let txt = wrapAnsi(msg.trim().replace(/\t+/g, ""), width - 7, {hard: true});
+  let width = cliWidth({defaultWidth: 80}) - 1;
+  let txt = wrapAnsi(msg.replace(/\t+/g, ""), width - 6, {hard: true});
   
   console.log(boxen(chalk`{red ${title.trim()}}\n\n${txt}`, boxOptions));
 }
 
 function warningMsg(title, msg) {
-  let width = cliWidth({defaultWidth: 80});
-  //let txt = wrapAnsi(msg.trim().replace(/\t+/g, ""), width - 7, {hard: true});
-  let txt = wrapAnsi(
-    chalk`{yellowBright ${title.trim()}}\n\n${msg.replace(/\t+/g, "").trim()}`,
-    width - 7
-  );
+  let width = cliWidth({defaultWidth: 80}) - 1;
+  let txt = wrapAnsi(msg.replace(/\t+/g, ""), width - 6, {hard: true});
   
-  //console.log(boxen(chalk`{yellowBright ${title.trim()}}\n\n${txt}`, boxOptions));
-  console.log(boxen(txt, boxOptions));
+  console.log(boxen(chalk`{yellowBright ${title.trim()}}\n\n${txt}`, boxOptions));
 }
 
 function infoMsg(title, msg) {
-  let width = cliWidth({defaultWidth: 80});
-  let txt = wrapAnsi(msg.trim().replace(/\t+/g, ""), width - 7, {hard: true});
+  let width = cliWidth({defaultWidth: 80}) - 1;
+  let txt = wrapAnsi(msg.replace(/\t+/g, ""), width - 6, {hard: true});
 
   console.log(boxen(chalk`{cyan ${title.trim()}}\n\n${txt}`, boxOptions));
 }
