e3a0f2515d87a76ee0310e1a3951ec347ac07175
Tim Cross <theophilusx@gmail.com>
2019-01-30 14:42:33 +1100

    Updated approvalsUI to work with new object definitions

--- src/tui/approvalsUI.js
+++ src/tui/approvalsUI.js
@@ -3,15 +3,11 @@
 const moduleName = "approvalsUI";
 
 const VError = require("verror");
-const approvals = require("./approvals");
 const menu = require("./textMenus");
 const screen = require("./textScreen");
 const planui = require("./planUI");
-const git = require("./git");
 const path = require("path");
 const edit = require("./edit");
-const plans = require("./plans");
-const inquirer = require("inquirer");
 const gitui = require("./gitUI");
 const rejectui = require("./rejectUI");
 
@@ -34,7 +30,7 @@ function approvalActions(state) {
       switch (answer.choice) {
       case "viewPlan": {
         let choice = "";
-        [state, choice] = await planui.selectPlan(state, "pendingPlans");
+        [state, choice] = await planui.selectPlan(state, "Pending");
         if (menu.doExit(choice)) {
           return state;
         }
@@ -49,25 +45,26 @@ function approvalActions(state) {
       }
       case "comparePlan": {
         let planId;
-        [state, planId] = await planui.selectPlan(state, "pendingPlans");
+        [state, planId] = await planui.selectPlan(state, "Pending");
         if (menu.doExit(planId)) {
           return state;
         }
         let planDef = state.currentPlanDef();
-        let commitHistory = await git.fileHistory(state, planDef.change);
-        gitui.displayCommitHistory(commitHistory);
-        let diffList = await git.fileDiff(state, commitHistory[0].commit.sha());
+        let repo = state.currentRepositoryDef();
+        let history = await repo.gitRepo.fileHistory(planDef.change);
+        gitui.displayCommitHistory(history);
+        let diffList = await repo.gitRepo.fileDiff(history[0].commit.sha());
         await gitui.displayDiff(diffList);
         break;
       }
       case "approvePlan": {
         let choice;
-        [state, choice] = await planui.selectPlan(state, "pendingPlans");
+        [state, choice] = await planui.selectPlan(state, "Pending");
         if (menu.doExit(choice)) {
           return state;
         }
-        if (state.approvalType !== "none"
-            && !approvals.isApprover(state)) {
+        let repoDef = state.currentRepositoryDef();
+        if (!repoDef.isApprover(state.email())) {
           screen.errorMsg(
             "Not Approved",
             "You are not one of the registered approvers "
@@ -75,24 +72,25 @@ function approvalActions(state) {
           );
           return state;
         }
-        state = await approvals.addApproval(state);
-        state = plans.movePlanToApproved(state);
-        let repo = state.get("repoObject");
-        await git.createBranch(repo, "approvals");
-        await repo.checkoutBranch("approvals");
-        await plans.writePlanFiles(state);
-        await state.writeConfigFile();
-        let changeFiles = await repo.getStatus();
-        if (changeFiles.length) {
-          let planDef = state.currentPlanDef();
-          let commitMsg = `Committing approval for change plan '${planDef.name}'`;
-          await git.addAndCommit(state, changeFiles, commitMsg);
-          await git.pullMaster(repo);
-          await git.mergeBranchIntoMaster(state, "approvals");
-        } else {
-          await git.deleteBranch(repo, "approvals");        
+        repoDef.gitRepo.createBranch("approval");
+        repoDef.gitRepo.checkout("approval");
+        let planDef = state.currentPlanDef();
+        let sha = repoDef.gitRepo.getChangesSha(planDef);
+        planDef.addApproval(state.username(), state.email(), sha);
+        if (repoDef.approvalType === "any"
+            || (repoDef.approvalType === "all"
+                && repoDef.approvers.size === planDef.currentApprovalCount())) {
+          planDef.setCurrentApprovalState(true, sha);
+          planDef.setType("Approved");
         }
-        break;
+        await state.writeChangePlans();
+        await repoDef.commitAndMerge(
+          "approval",
+          `Approval of plan ${planDef.name}`,
+          state.username(),
+          state.email()
+        );
+        return state;
       }
       case "rejectPlan":
         state = rejectui.rejectPlan(state);
@@ -130,126 +128,125 @@ async function processPlanApproval(state) {
   }
 }
 
-async function selectApprovalType(state) {
-  const logName = `${moduleName}.selectApprovalType`;
-  const choices = [{
-    name: "No approvals required",
-    value: "none"
-  },
-  {
-    name: "Any single registered approver can approve a change",
-    value: "any"
-  },
-  {
-    name: "All registered approvers must approve the change",
-    value: "all"
-  }];
+// async function selectApprovalType(state) {
+//   const logName = `${moduleName}.selectApprovalType`;
+//   const choices = [{
+//     name: "No approvals required",
+//     value: "none"
+//   },
+//   {
+//     name: "Any single registered approver can approve a change",
+//     value: "any"
+//   },
+//   {
+//     name: "All registered approvers must approve the change",
+//     value: "all"
+//   }];
   
-  try {
-    console.log(`Current Approval Type: ${state.approvalType()}`);
-    let choice = await menu.listMenu(
-      state,
-      "Approval Type",
-      "Select approval type:",
-      choices
-    );
-    state.setApprovalType(choice);
-    return state;
-  } catch (err) {
-    throw new VError(err, `${logName} `);
-  }
-}
+//   try {
+//     console.log(`Current Approval Type: ${state.approvalType()}`);
+//     let choice = await menu.listMenu(
+//       state,
+//       "Approval Type",
+//       "Select approval type:",
+//       choices
+//     );
+//     state.setApprovalType(choice);
+//     return state;
+//   } catch (err) {
+//     throw new VError(err, `${logName} `);
+//   }
+// }
 
-async function selectApprovers(state) {
-  const logName = `${moduleName}.selectApprovers`;
+// async function selectApprovers(state) {
+//   const logName = `${moduleName}.selectApprovers`;
 
-  try {
-    let choices = [];
-    let approvers = state.approvers();
-    for (let a of approvers.keys()) {
-      choices.push({
-        name: `${approvers.get(a)} <${a}>`,
-        value: a
-      });
-    }
-    let question1 = [{
-      type: "checkbox",
-      name: "toKeep",
-      choices: choices,
-      message: "Select approvers to keep:"
-    }];
-    let question2 = [{
-      type: "confirm",
-      name: "addMore",
-      message: "Do you want to add another approver:"
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "Approver's name:",
-      when: answer => {
-        return answer.addMore;
-      }
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Approver's email address:",
-      when: answer => {
-        return answer.addMore;
-      }
-    }];
-    let newApprovers = new Map();
-    if (approvers.size > 0) {
-      let answer = await inquirer.prompt(question1);
-      for (let a of answer.toKeep) {
-        newApprovers.set(a, approvers.get(a));
-      }
-    }
-    let answer;
-    do {
-      answer = await inquirer.prompt(question2);
-      if (answer.addMore) {
-        newApprovers.set(answer.email, answer.name);
-      }
-    } while (answer.addMore);
-    state.setApprovers(newApprovers);
-    return state;
-  } catch (err) {
-    throw new VError(err, `${logName} `);
-  }
-}
+//   try {
+//     let choices = [];
+//     let approvers = state.approvers();
+//     for (let a of approvers.keys()) {
+//       choices.push({
+//         name: `${approvers.get(a)} <${a}>`,
+//         value: a
+//       });
+//     }
+//     let question1 = [{
+//       type: "checkbox",
+//       name: "toKeep",
+//       choices: choices,
+//       message: "Select approvers to keep:"
+//     }];
+//     let question2 = [{
+//       type: "confirm",
+//       name: "addMore",
+//       message: "Do you want to add another approver:"
+//     },
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "Approver's name:",
+//       when: answer => {
+//         return answer.addMore;
+//       }
+//     },
+//     {
+//       type: "input",
+//       name: "email",
+//       message: "Approver's email address:",
+//       when: answer => {
+//         return answer.addMore;
+//       }
+//     }];
+//     let newApprovers = new Map();
+//     if (approvers.size > 0) {
+//       let answer = await inquirer.prompt(question1);
+//       for (let a of answer.toKeep) {
+//         newApprovers.set(a, approvers.get(a));
+//       }
+//     }
+//     let answer;
+//     do {
+//       answer = await inquirer.prompt(question2);
+//       if (answer.addMore) {
+//         newApprovers.set(answer.email, answer.name);
+//       }
+//     } while (answer.addMore);
+//     state.setApprovers(newApprovers);
+//     return state;
+//   } catch (err) {
+//     throw new VError(err, `${logName} `);
+//   }
+// }
 
-async function editApprovalSettings(state) {
-  const logName = `${moduleName}.editApprovals`;
+// async function editApprovalSettings(state) {
+//   const logName = `${moduleName}.editApprovals`;
 
-  try {
-    let committed = await gitui.commitChanges(state);
-    if (committed) {
-      let repo = state.get("repoObject");
-      await git.createBranch(repo, "approvals");
-      await repo.checkoutBranch("approvals");
-      state = await selectApprovalType(state);
-      state = await selectApprovers(state);
-      await approvals.writeApprovalsFile(state);
-      let changeFiles = await repo.getStatus();
-      if (changeFiles.length) {
-        let commitMsg = "Updating repository approval settings";
-        await git.addAndCommit(state, changeFiles, commitMsg);
-        await git.pullMaster(repo);
-        await git.mergeBranchIntoMaster(state, "approvals");
-      } else {
-        await git.deleteBranch(repo, "approvals");
-      }
-      return state;
-    }
-  } catch (err) {
-    throw new VError(err, `${logName} `);
-  }
-}
+//   try {
+//     let committed = await gitui.commitChanges(state);
+//     if (committed) {
+//       let repo = state.get("repoObject");
+//       await git.createBranch(repo, "approvals");
+//       await repo.checkoutBranch("approvals");
+//       state = await selectApprovalType(state);
+//       state = await selectApprovers(state);
+//       await approvals.writeApprovalsFile(state);
+//       let changeFiles = await repo.getStatus();
+//       if (changeFiles.length) {
+//         let commitMsg = "Updating repository approval settings";
+//         await git.addAndCommit(state, changeFiles, commitMsg);
+//         await git.pullMaster(repo);
+//         await git.mergeBranchIntoMaster(state, "approvals");
+//       } else {
+//         await git.deleteBranch(repo, "approvals");
+//       }
+//       return state;
+//     }
+//   } catch (err) {
+//     throw new VError(err, `${logName} `);
+//   }
+// }
 
 module.exports = {
   processPlanApproval,
-  editApprovalSettings
 };
 
