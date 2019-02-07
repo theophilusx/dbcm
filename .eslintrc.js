7ff67be3e29729bc163707b1125ec954e47c723a
Tim Cross <theophilusx@gmail.com>
2019-02-08 10:05:45 +1100

    Added some housekeeping files to support dev modes

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-unused-vars": [
+      "error",
+      {
+        "varsIgnorePattern": "should|expect"
+      }
+    ],
+    "no-console": "off",
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
