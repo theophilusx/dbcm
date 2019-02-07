26f9213c57cc3fbe0e48af33e416b052f46ada31
Tim Cross <theophilusx@gmail.com>
2019-02-07 14:36:09 +1100

    Added commitHistory module

---
+++ src/tui/commitHistory.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const VError = require("verror");
+const selectPlan = require("./selectPlan");
+const gitui = require("./gitUI");
+
+async function commitHistory(state, group) {
+  const logName = "commitHistory";
+  let choice;
+  
+  try {
+    [state, choice] = await selectPlan(state, group);
+    if (choice) {
+      let repo = state.currentRepositoryDef();
+      let plan = state.planDef(choice);
+      let hist = await repo.gitRepo.fileHistory(plan.change);
+      gitui.displayCommitHistory(hist);
+    }
+    return state;
+  } catch (err) {
+    throw new VError(err, `${logName}`);
+  }
+}
+
+module.exports = commitHistory;
