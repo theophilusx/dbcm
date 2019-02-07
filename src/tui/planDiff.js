1a6f48f6a8e2406bd381ad161b791c931aa449ba
Tim Cross <theophilusx@gmail.com>
2019-02-07 14:36:35 +1100

    Added planDiff module

---
+++ src/tui/planDiff.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const VError = require("verror");
+const selectPlan = require("./selectPlan");
+const gitui = require("./gitUI");
+
+async function planDiff(state, group) {
+  const logName = "planDiff";
+  let choice;
+  
+  try {
+    [state, choice] = await selectPlan(state, group);
+    if (choice) {
+      let repo = state.currentRepositoryDef();
+      let plan = state.planDef(choice);
+      let hist = await repo.gitRepo.fileHistory(plan.change);
+      let diffList = await repo.gitRepo.fileDiff(hist[0].commit.sha());
+      await gitui.displayDiff(diffList);
+    }
+    return state;
+  } catch (err) {
+    throw new VError(err, `${logName}`);
+  }
+}
+
+module.exports = planDiff;
