13caf194f34b5830db3fc906bfd5b409342c5045
Tim Cross <theophilusx@gmail.com>
2019-02-07 13:20:29 +1100

    Create submitPlan module

---
+++ src/tui/submitPlan.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const VError = require("verror");
+const screen = require("./textScreen");
+const menu = require("./textMenus");
+const selectPlan = require("./selectPlan");
+
+async function submitPlan(state) {
+  const logName = "submitPlan";
+  let choice;
+  
+  try {
+    [state, choice] = await selectPlan(state, "Development");
+    if (choice) {
+      let branch = `${process.env.USER}-local`;
+      let repo = state.currentRepositoryDef();
+      await repo.gitRepo.checkoutBranch(branch);
+      let plan = state.currentPlanDef();
+      plan.textDisplay();
+      let doSubmit = await menu.confirmMenu("Submit Plan", "Submit this plan for approval");
+      if (doSubmit) {
+        state.setCurrentPlanType("Pending");
+        await state.writeChangePlans();
+        await repo.commitAndMerge(
+          branch,
+          `Submitting plan ${plan.name} for approval`,
+          state.username(),
+          state.email()
+        );
+      } else {
+        screen.infoMsg("Cancelled", "Plan approval submission cancelled");
+      }
+    }
+    return state;
+  } catch (err) {
+    throw new VError(err, `${logName} Failed to move plan to pending gorup`);
+  }
+}
+
+module.exports = submitPlan;
+
