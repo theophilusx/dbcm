853b4743ec8736ddfe55bc485ae8491924f4a70c
Tim Cross <theophilusx@gmail.com>
2019-02-08 12:05:27 +1100

    Add missing await statements

--- src/tui/targets/rollbackLastChange.js
+++ src/tui/targets/rollbackLastChange.js
@@ -9,14 +9,15 @@ async function rollbackLastChange(state) {
 
   try {
     let target = state.currentTargetDef();
-    let appliedPlans = target.getAppliedPlans();
+    let appliedPlans = await target.appliedPlans();
     let lastPlan = appliedPlans.pop();
     let plan = state.planDef(lastPlan.uuid);
     plan.textDisplay();
-    let doRollback = menu.confirmMenu("Rollback Change", "Rollback this plan");
+    let doRollback = await menu.confirmMenu("Rollback Change", "Rollback this plan");
     if (doRollback) {
       await psql.rollbackPlan(state, plan);
     }
+    return state;
   } catch (err) {
     throw new VError(err, `${logName}`);
   }
