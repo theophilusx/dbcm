f7c656604d82afc4b7725fad82f95be3c7cdb24a
Tim Cross <theophilusx@gmail.com>
2019-02-08 15:26:10 +1100

    Add ability to add note when submitting plan for approval

--- src/tui/plans/submitPlan.js
+++ src/tui/plans/submitPlan.js
@@ -4,6 +4,7 @@ const VError = require("verror");
 const screen = require("../utils/textScreen");
 const menu = require("../utils/textMenus");
 const selectPlan = require("./selectPlan");
+const createNote = require("./createNote");
 
 async function submitPlan(state) {
   const logName = "submitPlan";
@@ -12,17 +13,18 @@ async function submitPlan(state) {
   try {
     [state, choice] = await selectPlan(state, "Development");
     if (choice) {
-      if (choice !== state.currentPlanUUID()) {
-        state.setCurrentPlanUUID(choice);
-      }
       let branch = `${process.env.USER}-local`;
       let repo = state.currentRepositoryDef();
       await repo.gitRepo.checkoutBranch(branch);
-      let plan = state.currentPlanDef();
+      let plan = state.planDef(choice);
       plan.textDisplay();
       let doSubmit = await menu.confirmMenu("Submit Plan", "Submit this plan for approval");
       if (doSubmit) {
-        state.setCurrentPlanType("Pending");
+        plan.setType("Pending");
+        let doNote = await menu.confirmMenu("Promote Note", "Do you want to add a promote note");
+        if (doNote) {
+          await createNote(state, plan, "Promote Note", "Enter notes for this change");
+        }
         await state.writeChangePlans();
         await repo.commitAndMerge(
           branch,
