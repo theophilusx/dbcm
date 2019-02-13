e4a04cee16a03c455303c0edd8ac4ace9571dbbf
Tim Cross <theophilusx@gmail.com>
2019-02-13 12:41:08 +1100

    Cleanup index.js

--- src/index.js
+++ src/index.js
@@ -9,44 +9,6 @@ const path = require("path");
 const selectTarget = require("./tui/targets/selectTarget");
 const mainui = require("./tui/mainUI");
 
-// const repoui = require("./repoUI");
-// const configui = require("./configUI");
-// const state = require("./state");
-
-// const git = require("./git");
-// const plans = require("./plans");
-// const approvals = require("./approvals");
-// const menu = require("./textMenus");
-
-// async function main() {
-//   let appState;
-
-//   try {
-//     appState = await state.createApplicationState();
-//     if (!appState.username()) {
-//       appState = await configui.getConfig(appState);
-//     }
-//     if (!appState.currentRepository()) {
-//       appState = await repoui.selectRepository(appState);
-//     }
-//     if (menu.doExit(appState.menuChoice())) {
-//       process.exit();
-//     }
-//     appState = await git.setupRepository(appState);
-//     appState = await plans.readPlanFiles(appState);
-//     appState = await approvals.readApprovalsFile(appState);
-//     if (!appState.currentTarget()) {
-//       appState = await targetui.selectTarget(appState);
-//     }
-//     await appState.writeConfigFile();
-//     appState = await mainui.mainMenu(appState);
-//     console.log("Exiting DBCM");
-//     await appState.writeConfigFile();
-//   } catch (err) {
-//     throw new VError(err, "Main loop error");
-//   }
-// }
-
 async function main() {
   const logName = "main";
   const appState = new AppState();
@@ -61,12 +23,13 @@ async function main() {
     await selectRepository(appState);
     await selectTarget(appState);
     await mainui.mainMenu(appState);
-    console.log(dumper.dumpValue(appState, "", "appState"));
   } catch (err) {
+    console.log(dumper.dumpValue(appState, "", "appState"));
     throw new VError(err, `${logName}`);
   }
 }
 
-main().catch(err => {
-  console.error(err.message);
-});
+main()
+  .catch(err => {
+    console.error(err.message);
+  });
