798926d2c1881bbae4f4f39b488dd1843ce7ad08
Tim Cross <theophilusx@gmail.com>
2019-02-14 15:44:32 +1100

    Prompt before applying rollback after failure

--- src/tui/targets/applyChange.js
+++ src/tui/targets/applyChange.js
@@ -3,19 +3,27 @@
 const VError = require("verror");
 const selectPlan = require("../plans/selectPlan");
 const psql = require("../../psql");
+const menu = require("../utils/textMenus");
 
 async function applyChange(state, group) {
   const logName = "applyChange";
   let choice;
-  
+
   try {
     [state, choice] = await selectPlan(state, group);
     if (choice) {
       let applied = await psql.applyPlan(state, state.planDef(choice));
       if (applied) {
         await psql.verifyPlan(state, state.planDef(choice));
-      } else {                    // change plan applied with errors
-        await psql.rollbackPlan(state, state.planDef(choice));
+      } else {
+        // change plan applied with errors
+        let doRollback = await menu.confirmMenu(
+          "Rollback Plan",
+          "Change encountered errors. Do you want to roll it back"
+        );
+        if (doRollback) {
+          await psql.rollbackPlan(state, state.planDef(choice));
+        }
       }
     }
     return state;
