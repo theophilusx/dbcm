5723f891a2c16223b44eddc96ca4549bbd27d41d
Tim Cross <theophilusx@gmail.com>
2019-02-15 10:44:05 +1100

    Update to use generic markPlan module

--- src/tui/targets/markPlanApplied.js
+++ src/tui/targets/markPlanApplied.js
@@ -1,30 +1,13 @@
 "use strict";
 
 const VError = require("verror");
-const selectPlan = require("../plans/selectPlan");
-const menu = require("../utils/textMenus");
-const queries = require("../../database");
+const markPlan = require("./markPlan");
 
 async function markPlanApplied(state) {
   const logName = "markPlanApplied";
-  let planId;
 
   try {
-    [state, planId] = await selectPlan(state, "Approved");
-    if (planId) {
-      let plan = state.planDef(planId);
-      plan.textDisplay();
-      let doMark = await menu.confirmMenu(
-        "Mark Plan Applied",
-        `Mark this plan as applied in DB ${state.currentTargetName()}`
-      );
-      if (doMark) {
-        let planSHA = await state
-          .currentRepositoryDef()
-          .gitRepo.getChangeFileSHA(plan);
-        await queries.updateAppliedPlanStatus(state, plan, "Applied", planSHA);
-      }
-    }
+    await markPlan(state, "Applied");
     return state;
   } catch (err) {
     throw new VError(err, `${logName}`);
