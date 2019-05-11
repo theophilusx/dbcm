97ec6efb3f7235a184971985602487a703c23b93
Tim Cross <theophilusx@gmail.com>
2019-05-11 23:29:25 +1000

    Restrict rollback change to applied plans

--- src/tui/targets/rollbackChange.js
+++ src/tui/targets/rollbackChange.js
@@ -9,7 +9,12 @@ async function rollbackChange(state, group) {
   const logName = "rollbackChange";
 
   try {
-    let planChoices = state.changePlans().planGroupMap(group);
+    let definedPlans = state.changePlans().planGroupMap(group);
+    let repo = state.currentRepositoryDef();
+    let target = state.currentTargetDef();
+    let planChoices = await target.appliedPlans(repo, definedPlans);
+    console.log("planChoices");
+    console.dir(planChoices);
     let choice = await selectPlan(state, planChoices);
     if (choice) {
       let plan = state.planDef(choice);
@@ -24,7 +29,7 @@ async function rollbackChange(state, group) {
     }
     return state;
   } catch (err) {
-    throw new VError(err, `${logName}`);
+    throw new VError(err, logName);
   }
 }
 
