62ea5b0ff4daf659f955a081d4f119738aeffecb
Tim Cross <theophilusx@gmail.com>
2019-05-12 13:41:23 +1000

    Check plan exists in current group

--- src/Target.js
+++ src/Target.js
@@ -92,12 +92,18 @@ Target.prototype.appliedPlans = async function(repo, definedPlans) {
 
   try {
     let dbAppliedPlans = await queries.getAppliedPlans(this);
+    console.log("dbAppliedPlans");
+    console.dir(dbAppliedPlans);
     let applied = new Map();
     for (let a of dbAppliedPlans) {
-      let plan = definedPlans.get(a.uuid);
-      let currentSHA = await repo.gitRepo.getChangeFileSHA(plan);
-      if (a.changeSHA === currentSHA) {
-        applied.set(a.uuid, definedPlans.get(a.uuid));
+      if (definedPlans.has(a.uuid)) {
+        let plan = definedPlans.get(a.uuid);
+        console.log("plan");
+        console.dir(plan);
+        let currentSHA = await repo.gitRepo.getChangeFileSHA(plan);
+        if (a.changeSHA === currentSHA) {
+          applied.set(a.uuid, definedPlans.get(a.uuid));
+        }
       }
     }
     return applied;
